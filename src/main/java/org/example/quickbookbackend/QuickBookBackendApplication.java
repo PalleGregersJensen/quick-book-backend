60d98f57bdc6c96d454f91e93d8f814744fb8bd7
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2024-06-17 11:05:37 +0200

    Repository created

---
+++ src/main/java/org/example/quickbookbackend/QuickBookBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.quickbookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickBookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickBookBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/quickbookbackend/QuickBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quickbookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickBookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
